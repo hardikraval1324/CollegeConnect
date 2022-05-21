7e1238c0bd7974020723e70a450a546b1b6dbc37
hardikraval1324 <hardik1324@gmail.com>
2022-05-21 21:32:01 +0530

    signup2 eror solved

--- app/src/main/java/com/college/collegeconnect/activities/StepTwoSignUp.java
+++ app/src/main/java/com/college/collegeconnect/activities/StepTwoSignUp.java
@@ -80,11 +80,16 @@ public class StepTwoSignUp extends AppCompatActivity {
 
         databaseReference = firebaseDatabase.getReference("Colleges");
         databaseReference.addListenerForSingleValueEvent(valueListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            public void onCreate(@NonNull DataSnapshot snapshot) {
                 ArrayList<String> arrayList2 = (ArrayList<String>) snapshot.getValue();
                 arrayList.addAll(arrayList2);
                 spinnerArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
             }
 
             @Override
